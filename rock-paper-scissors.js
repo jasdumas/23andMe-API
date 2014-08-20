e548e715816d0731980f01552657ab16fe049cc8
Jasmine Dumas <jasmine.dumas@gmail.com>
2014-08-20 17:07:55 -0400

    Create rock-paper-scissors.js

---
+++ rock-paper-scissors.js
@@ -0,0 +1,45 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+var compare = function(choice1, choice2) {
+if(choice1 === choice2) {
+    return "The result is a tie";
+}
+
+else if(choice1 === "rock") {
+
+    if(choice2 === "scissors") {
+        return "rock wins";
+    }
+    else {
+        return "paper wins";
+    }
+if (choice1 === "paper") {
+  return "paper wins"
+}
+else if (choice2 ==="rock") {
+  if (condition) {
+    return "paper wins"
+  } 
+  else {
+    return "scissors wins"
+  }
+} 
+if (choice2 === "rock") {
+  return "rock wins"
+}
+else if (choice2 ==="paper") {
+  if (condition) {
+    return "scissors wins"
+  } 
+  else {
+    return "scissors wins"
+  }
+}}};
+compare(userChoice,computerChoice);
